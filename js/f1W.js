7a84719890ac605320ddc3a7916528fe5e7607eb
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2024-03-26 17:50:20 -0400

    img

--- js/f1W.js
+++ js/f1W.js
@@ -1,73 +1,90 @@
+function mostrarnavbar() {
+    const submenu = document.getElementById("navbar");
+    submenu.classList.toggle("hidden");
+}
+const bar = document.querySelectorAll("#navbar");
+bar.forEach((bar) => {
+    bar.addEventListener("click", () => {
+        mostrarsubmenu();
+    });
+})
 function mostrarinfoRB() {
     const submenu = document.getElementById("RedBull");
     submenu.classList.toggle("hidden");
 }
-
 const elementos = document.querySelectorAll("#RedBull");
 elementos.forEach((elemento) => {
     elemento.addEventListener("click", () => {
         mostrarsubmenu();
     });
 })
-
 function mostrarinfoMercedes() {
     const submenu = document.getElementById("Mercedes");
     submenu.classList.toggle("hidden");
 }
-
 const element = document.querySelectorAll("#Mercedes");
 element.forEach((elemento) => {
     elemento.addEventListener("click", () => {
         mostrarsubmenu();
     });
 })
-
 function mostrarinfoFerrari() {
     const submenu = document.getElementById("Ferrari");
     submenu.classList.toggle("hidden");
 }
-
 const elemento = document.querySelectorAll("#Ferrari");
 elemento.forEach((elemento) => {
     elemento.addEventListener("click", () => {
         mostrarsubmenu();
     });
 })
-
 function mostrarinfomclaren() {
     const submenu = document.getElementById("mclaren");
     submenu.classList.toggle("hidden");
 }
-
 const elements = document.querySelectorAll("#mclaren");
 elements.forEach((elemento) => {
     elemento.addEventListener("click", () => {
         mostrarsubmenu();
     });
 })
-
 function mostrarinfoaston() {
     const submenu = document.getElementById("aston");
     submenu.classList.toggle("hidden");
 }
-
 const elemen = document.querySelectorAll("#aston");
 elemen.forEach((elemento) => {
     elemento.addEventListener("click", () => {
         mostrarsubmenu();
     });
 })
-
 function mostrarinfoalpine() {
     const submenu = document.getElementById("alpine");
     submenu.classList.toggle("hidden");
 }
-
 const elemens = document.querySelectorAll("#alpine");
 elemens.forEach((elemento) => {
     elemento.addEventListener("click", () => {
         mostrarsubmenu();
     });
 })
-
-
+function mostrarinfoVCA() {
+    const submenu = document.getElementById("RB");
+    submenu.classList.toggle("hidden");
+}
+const menu = document.querySelectorAll("#RB");
+menu.forEach((elemento) => {
+    elemento.addEventListener("click", () => {
+        mostrarsubmenu();
+    });
+})
+function mostrarinfohaas() {
+    const submenu = document.getElementById("haas");
+    submenu.classList.toggle("hidden");
+}
+const info = document.querySelectorAll("#haas");
+info.forEach((elemento) => {
+    elemento.addEventListener("click", () => {
+        mostrarsubmenu();
+    });
+})
